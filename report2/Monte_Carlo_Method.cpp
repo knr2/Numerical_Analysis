becc3941d85e6ddc4abd87ac8b509f94e5667d43
nakamura ryo <60230025+knr2@users.noreply.github.com>
2020-12-18 15:52:29 +0900

    Update Monte_Carlo_Method.cpp

--- report2/Monte_Carlo_Method.cpp
+++ report2/Monte_Carlo_Method.cpp
@@ -1,12 +1,13 @@
 #include <iomanip>      // setprecsion
 #include <iostream>     // std::cout, std::endl, std::cin
+#include <vector>     // std::cout, std::endl, std::cin
 #include <cstdio>       // printf
 #include <cstdlib>      // rand, std::srand
 #include <cmath>        // sin, cos, tan
 
 // 変数定義
-const long long count = 1000000;            // 座標の個数を指定
-const long long size = 10;                  // 正方形の大きさを指定
+long long count = 1;            // 座標の個数を指定
+int size = 2;                  // 正方形の大きさを指定
 const long double PI = acos(-1.0);          // 円周率
 long long area_true_count = 0;              // 円内に入ってた回数
 long double area_PI;                        // 面積から求めた確率
@@ -18,17 +19,51 @@ void area(long long, long long);            // 面積より
 void output(void);                          // 出力
 // ここまで
 
+std::vector<int> plot ={2,4,8,10,12,20,50,100};
+
 int main(void)
 {
     setting();
-    for (long long i = 0; i < count; i++)
+    FILE *fp1;
+    if ((fp1 = fopen("answer.txt", "w")) == NULL)
     {
-        // (x, y)を乱数により指定
-        long long x = rand() % size;
-        long long y = rand() % size;
-        area(x, y);
+        fprintf(stderr, "Can not find Sentence.txt\n");
+        exit(1);
     }
-    output();
+    for(int k = 0;k < plot.size();k++)
+    {
+        size = plot[k];
+        fprintf(fp1, "\nN = %d\n", size);
+        count = 1;
+        for(int j = 0;j < 24;j++)
+        {
+            count *= 2;
+            area_true_count = 0;
+            for (long long i = 0; i < count; i++)
+            {
+                // (x, y)を乱数により指定
+                long long x = rand() % size;
+                long long y = rand() % size;
+                area(x, y);
+            }
+            
+            // 出力したいとき
+            // output();
+
+            // ファイルへ書き込む場合
+            area_PI = (area_true_count * 1.) / (count * 1.) * 4;
+            if(j % 2 == 0)
+            {
+                fprintf(fp1, "2の%d乗\t%.10Lf\t\t", j + 1, area_PI);
+                std::cout << area_PI << std::endl;
+            }
+            else
+            {
+                fprintf(fp1, "2の%d乗\t%.10Lf\n", j + 1, area_PI);
+                std::cout << area_PI << std::endl;
+            }
+        }
+    }
     return 0;
 }
 
@@ -49,7 +84,7 @@ void area(long long x, long long y)
 // 出力
 void output(void)
 {
-    area_PI = (area_true_count * 1.) / (count * 1.) * 100 / (size / 2.) / (size / 2.);
+    area_PI = (area_true_count * 1.) / (count * 1.) * 4;
     std::cout << "模範解" << std::endl << PI << std::endl;
     std::cout << "円の面積から求めた解" << std::endl << area_PI << std::endl << "誤差 = " << fabs(PI - area_PI) << std::endl;
 }
